ab33c59bf72b56a218136db3bf9b938a8e1808c6
EdoPi <edoardo.pizzicannella@gmail.com>
2021-03-22 18:10:16 +0100

    modale caricata

--- resources/views/Beers/beers.blade.php
+++ resources/views/Beers/beers.blade.php
@@ -4,9 +4,6 @@
 
 @section('main-content')
 
-@php
-    dump($beers);
-@endphp
 <div class="background-list">
 
 </div>
@@ -28,37 +25,37 @@
                 <tr class="list-table beers-table">
                     <td scope="row">{{$beers->id}}</td>
                     <td><a href="beers/{{$beers->id}}">{{$beers->name}}</a></td>
-                    <td>{{$beers->alchool_graduation}} °</td>
+                    <td>{{$beers->alchool_graduation}} %</td>
                     <td>{{$beers->fermentation_type}}</td>
                     <td>{{$beers->color}}</td>
                     <td><a href="beers/{{$beers->id}}"><img src="{{$beers->image}}" alt="" style="height: 100px"></a></td>
                     <td>
-                        <button class="btn btn-success action-button"><a href="beers/{{$beers->id}}" role="button">DETAIL</a></button>
-                        <button class="btn btn-primary" type="submit" value="submit"><a href="beers/{{$beers->id}}/edit">EDIT</a></button>
+                        <a class="btn btn-success" href="beers/{{$beers->id}}" role="button">DETAIL</a>
+                        <a class="btn btn-primary" href="beers/{{$beers->id}}/edit">EDIT</a>
                         <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modal-{{$beers->id}}">
                             DELETE
                         </button>
 
                         <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal fade" id="modal-{{$beers->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <h5 class="modal-title" id="exampleModalLabel"></h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                                 </div>
                                 <div class="modal-body">
-                                Sei sicuro di voler eliminare il Prodotto?
+                                SEI SICURO DI VOLER ELIMINARE IL PRODOTTO ?
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                                     <form class ="delete" action="{{route('beers.destroy', ['beer'=> $beers->id])}}" method="POST">
                                         @csrf
                                         @method('DELETE')
-                                        <button class="btn btn-danger" type="submit" value="submit">{{$beers->id}}DELETE</button>
+                                        <button class="btn btn-danger" type="submit" value="submit">DELETE</button>
                                     </form>
                                 </div>
                             </div>
