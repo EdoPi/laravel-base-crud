539a35a0d23de0f5ea32aa99a0c0c188eea12996
EdoPi <edoardo.pizzicannella@gmail.com>
2021-03-22 17:12:50 +0100

    inizio modale

--- resources/views/Beers/beers.blade.php
+++ resources/views/Beers/beers.blade.php
@@ -3,6 +3,10 @@
 @extends('layout')
 
 @section('main-content')
+
+@php
+    dump($beers);
+@endphp
 <div class="background-list">
 
 </div>
@@ -31,11 +35,35 @@
                     <td>
                         <button class="btn btn-success action-button"><a href="beers/{{$beers->id}}" role="button">DETAIL</a></button>
                         <button class="btn btn-primary" type="submit" value="submit"><a href="beers/{{$beers->id}}/edit">EDIT</a></button>
-                        <form class ="delete" action="{{route('beers.destroy', ['beer'=> $beers->id])}}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-danger" type="submit" value="submit">DELETE</button>
-                        </form>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                            DELETE
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                </div>
+                                <div class="modal-body">
+                                Sei sicuro di voler eliminare il Prodotto?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <form class ="delete" action="{{route('beers.destroy', ['beer'=> $beers->id])}}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger" type="submit" value="submit">{{$beers->id}}DELETE</button>
+                                    </form>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
                     </td>
                 </tr>
             @endforeach
